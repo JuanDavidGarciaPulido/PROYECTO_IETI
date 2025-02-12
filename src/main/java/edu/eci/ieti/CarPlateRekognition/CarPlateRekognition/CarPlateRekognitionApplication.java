005d08a4001068083b739911ccef8cf46cc27950
Juan David García Pulido <juan.david.garcia.pulido@gmail.com>
2025-02-12 13:03:54 -0500

    Initial configuration and dependencies

---
+++ src/main/java/edu/eci/ieti/CarPlateRekognition/CarPlateRekognition/CarPlateRekognitionApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.ieti.CarPlateRekognition.CarPlateRekognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarPlateRekognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarPlateRekognitionApplication.class, args);
+	}
+
+}
